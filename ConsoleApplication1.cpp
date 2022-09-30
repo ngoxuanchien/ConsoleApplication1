6f15057749cbaa0a21bc7c74243ee3fc901b37ef
ngoxuanchien <ngoxuanchien9a@gmail.com>
2022-09-30 16:16:37 +0700

    1

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -107,15 +107,8 @@ int ReadSector(LPCWSTR drive, int readPoint, BYTE sector[512])
 
 int main(int argc, char **argv)
 {
-    int cur;
     BYTE sector[512];
     ReadSector(L"\\\\.\\D:", 0, sector);
     // cout << sizeof(_fat32) / sizeof(_fat32[0]);
     return 0;
 }
-
-// chien ga
-// duong de thuong test ne
-//Hom nay la t6 va bay gio dang la 4h chieu, mot thoi diem thich hop de ngu trua, nhma lai phai vac mong^ len YES cafe de chay dl, nhma
-// van chua lam duoc cai khi gi ca, dung la mat thoi gian, gio ma ve nha ngu mot giac mo ve ngoi nha va nhung dua tre thi tot biet may
-//, haiz, dung la doi`,kkkk
