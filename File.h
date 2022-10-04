f66d664b083882cfdce46c73eba4be8d76ae175e
vanlyhai <lyhai1502@gmal.com>
2022-10-05 00:13:34 +0700

    Getter Setter Folder File

--- File.h
+++ File.h
@@ -15,14 +15,14 @@ public:
 	File();
 	~File();
 
-	void setName(const string&);
-	void setSize(const int&);
-	void setStatus(const bool&);
-	void setIndexSector(const int64_t&);
+	void setName(const string& name);
+	void setSize(const int& size);
+	void setStatus(const bool& status);
+	void setIndexSector(const int64_t& indexSector);
 
-	void getName();
-	void getSize();
-	void getStatus();
-	void getIndexSector();
+	string getName();
+	int getSize();
+	bool getStatus();
+	int64_t getIndexSector();
 };
 
